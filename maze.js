0e5f1c19e2e6782347b1ea3262aa7efa2ad47420
jordan <tengyou.chiu@tradevan.com.tw>
2019-08-16 14:45:21 +0800

    fix: 修正 _shuffleArray() 未正確洗牌的Bug

--- maze.js
+++ maze.js
@@ -99,7 +99,7 @@ class Maze{
 
     _shuffleArray(array){
         for(var i = array.length -1 ; i > 0;i--){
-            var j = Math.floor(Math.random(i+1));
+            var j = Math.floor(Math.random()*(i+1));
             var tmp = array[i];
             array[i] = array[j];
             array[j] = tmp;
