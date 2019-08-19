7efa32164067163eb112ae8549be579b70c60bd5
jordan <tengyou.chiu@tradevan.com.tw>
2019-08-19 11:47:26 +0800

    fix: 調整Maze大小

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var maze = new Maze();
-maze.create(4);
-console.log(maze);
-
+$(function(){
+    var maze = new Maze();
+    maze.create(30);
+    console.log(maze);
+    
+    var canvas = $("#canvas");
+    // canvas.height = 1000;
+    // canvas.width = 1000;
+    // let ctx = canvas.get(0).getContext("2d");
+    // //使用lineWidth指定線條寬度
+    // ctx.lineWidth = 3;
+    // //使用strokeStyle指定線條顏色
+    // ctx.strokeStyle = "#0000FF";
+    maze.draw(canvas);
+});
