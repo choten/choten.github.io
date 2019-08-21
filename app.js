ce289a39a279847b078b1acd9234bb63565b604d
jordan <tengyou.chiu@tradevan.com.tw>
2019-08-21 18:09:23 +0800

    feat: 增加 keyboard 上下左右 keydown 事件

--- app.js
+++ app.js
@@ -12,4 +12,52 @@ $(function(){
     // //使用strokeStyle指定線條顏色
     // ctx.strokeStyle = "#0000FF";
     maze.draw(canvas);
+    var POSTION = [0,0];
+
+    
+    window.goRight = function () {
+        if (!MAZE.hasWall(POS, Maze.RIGHT)) {
+          POS[0]++;
+          updateMyPosition();
+        }
+      }
+      window.goDown = function () {
+        if (!MAZE.hasWall(POS, Maze.BOTTOM)) {
+          POS[1]++;
+          updateMyPosition();
+        }
+      }
+      window.goLeft = function () {
+        if (!MAZE.hasWall(POS, Maze.LEFT)) {
+          POS[0]--;
+          updateMyPosition();
+        }
+      }
+      window.goUp = function () {
+        if (!MAZE.hasWall(POS, Maze.TOP)) {
+          POS[1]--;
+          updateMyPosition();
+        }
+      }
+    // Register key listeners
+    $(document).keydown(function (e) {
+        switch (e.keyCode) {
+            case 37: //left
+                goLeft();
+                break;
+            case 38: //up
+                goUp();
+                break;
+            case 39: //right
+                goRight();
+                break;
+            case 40: //down
+                goDown();
+                break;
+            default:
+                return;
+        }
+        // Disable document scrolling.
+        e.preventDefault();
+    });
 });
